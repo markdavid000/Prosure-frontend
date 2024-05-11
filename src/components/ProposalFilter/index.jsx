c8ec8d7e203196e017c233b0b524786178c01d05
markdavid000 <davidojukwu59@gmail.com>
2024-05-11 21:56:54 +0100

    Proposals page completed

--- src/components/ProposalFilter/index.jsx
+++ src/components/ProposalFilter/index.jsx
@@ -29,14 +29,20 @@ const ProposalFilter = () => {
           borderRadius="24px"
           border="1px solid #CFC4C5"
           width={'50%'}
-          _placeholder={{ color: '#fff' }}
+          _placeholder={{ color: '#e9ecef' }}
+          color={'#e9ecef'}
         />
       </InputGroup>
 
       <Text {...popFont}>FilterbyStatus:</Text>
 
       {/* Select one */}
-      <Select placeholder="All" _placeholder={{ color: '#fff' }} width={'20%'}>
+      <Select
+        placeholder="All"
+        _placeholder={{ color: '#e9ecef' }}
+        width={'20%'}
+        color={'#e9ecef'}
+      >
         <option value="option1">Option 1</option>
         <option value="option2">Option 2</option>
         <option value="option3">Option 3</option>
