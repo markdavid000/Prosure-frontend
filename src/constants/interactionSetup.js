09f250c8185218e7a4cc60d409eecbe9341c3c0e
Mona Lisarogue-in-anarchy <nehemiaheje@gmail.com>
2024-10-07 14:25:06 +0100

    updated to base

--- src/constants/interactionSetup.js
+++ src/constants/interactionSetup.js
@@ -4,11 +4,11 @@ import ERC20 from '../abi/ERC20Abi.json';
 
 // Deployed Contract Addresses
 
-export const mUSDT = '0x428B2A32171157C9A67b5e5283fd00Ca42fd54FE';
+export const mUSDT = '0x1F8ba6Ddc2D5086f13Df5b0AbB8833E6db6F71F6';
 
-export const prosureContract = '0x1A26f543e0c7E478C7D998109c0ACf7dB1827a6d';
+export const prosureContract = '0x54210cA1471b1064df6C525B5980a68361E5129e';
 
-export const governanceContract = '0xFeCe593B6eB317f4fD94804264742b092B4566c6';
+export const governanceContract = '0xC8fBEB1f56c7201E134F4870b0D3D03ce1497C68';
 
 export const prosureSetup = {
   address: prosureContract,
